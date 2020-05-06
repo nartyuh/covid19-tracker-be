a0dec9948a7415bb6cbffd5659392ce4af1599bc
Deku <dekutran@gmail.com>
2020-05-06 00:13:07 -0700

    update constraints

--- logs/models.py
+++ logs/models.py
@@ -12,12 +12,21 @@ class Log(models.Model):
     log_start = models.DateTimeField(null=False, blank=False)
     log_end = models.DateTimeField(null=False, blank=False)
 
+    class Meta:
+        unique_together = ('latitude', 'longitude', 'log_start', 'log_end',)
+
 
 class UserLog(models.Model):
     user = models.ForeignKey(User, to_field='username', null=False, blank=False, on_delete=models.CASCADE)
     log = models.ForeignKey(Log, null=False, blank=False, on_delete=models.CASCADE)
 
+    class Meta:
+        unique_together = ('user', 'log',)
+
 
 class TestPositive(models.Model):
     user = models.ForeignKey(User, to_field='username', null=False, blank=False, on_delete=models.CASCADE)
     date_tested = models.DateField(null=False, blank=False)
+
+    class Meta:
+        unique_together = ('user', 'date_tested',)
--- middlewares/middlewares.py
+++ middlewares/middlewares.py
@@ -7,6 +7,7 @@ class CorsMiddleware:
 
         response = self.get_response(request)
         response['Access-Control-Allow-Origin'] = 'https://covid19geotracker.tranquanghuy.me'
+        # response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
         response['Access-Control-Allow-Headers'] = 'Content-Type'
         response['Access-Control-Allow-Credentials'] = 'true'
         response['Access-Control-Allow-Methods'] = '*'
